ca06e77c52e550e3ea229fbcf4f016ec2dfad1b2
chandramohans <chandramohan.subbiah@gmail.com>
2016-02-02 09:43:56 +0530

    changed to dynamic port

--- Server.js
+++ Server.js
@@ -5,7 +5,8 @@ app.get('/', function (req, res) {
    res.send('Hello World');
 })
 
-var server = app.listen(8081, function () {
+
+var server = app.listen(process.env.PORT || 5000, function () {
 
   var host = server.address().address
   var port = server.address().port
