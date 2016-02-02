bf0d8552ab69ebb408e6cf0fcd14ea909ec05585
chandramohans <chandramohan.subbiah@gmail.com>
2016-02-02 10:53:54 +0530

    Added db connection details, REST.js

--- Server.js
+++ Server.js
@@ -1,16 +1,54 @@
-var express = require('express');
-var app = express();
+var express = require("express");
+var mysql   = require("mysql");
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+var rest = require("./REST.js");
+var app  = express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+function REST(){
+    var self = this;
+    self.connectMysql();
+};
 
+REST.prototype.connectMysql = function() {
+    var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 100,
+        host     : 'us-cdbr-iron-east-03.cleardb.net',
+        user     : 'b66276f8f3ed9f',
+        password : 'e3d4f6b4',
+        database : 'heroku_2f2b3584c2e81bb',
+        debug    :  false
+    });
+    pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+		  console.log('connected to db');
+        }
+    });
+}
 
-var server = app.listen(process.env.PORT || 5000, function () {
+REST.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new rest(router,connection,md5);
+      self.startServer();
+}
 
-  var host = server.address().address
-  var port = server.address().port
+REST.prototype.startServer = function() {
+      app.listen(process.env.PORT || 5000,function(){
+          console.log("All right ! I am alive at Port 3000.");
+      });
+}
 
-  console.log("Example app listening at http://%s:%s", host, port)
+REST.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL n" + err);
+    process.exit(1);
+}
 
-})
+new REST();
